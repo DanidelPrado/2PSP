dd37d76cb512069c1e95b4ef8c82227e5a029347
DanidelPrado <snait7@gmail.com>
2020-11-17 20:11:21 +0100

    Ejercicio 1 modificado y 3 lo que llevo

--- Examen_PSP/src/es/florida/App1.java
+++ Examen_PSP/src/es/florida/App1.java
@@ -1,24 +1,60 @@
 package es.florida;
 import java.io.*;
+import java.util.Scanner;
 
 public class App1 {
     public static void main(String[] args) {
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
+        String text="";
+        Scanner entradaScanner = null;
+        boolean contiene = false;
+        String line;
+        int num = 1;
+        Scanner scanner = new Scanner(System.in);
+        File f = new File("destino.txt");
         try {
-            File archivoOriginal = new File("origen.txt");
-            File archivoCopia = new File("destino.txt");
-            inputStream = new FileInputStream(archivoOriginal);
-            outputStream = new FileOutputStream(archivoCopia);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inputStream.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, length);
+            String ruta = System.getProperty("user.dir") + "origen.txt";
+            File d = new File(ruta);
+            entradaScanner = new Scanner(f);
+            while (entradaScanner.hasNext()) {
+                line = entradaScanner.nextLine();
+                if (!(line.contains("x")||(line.contains("z")))) {
+                    System.out.println(line);
+                    contiene = true;
+                    text = text + line+"\n";
+                }
+                num++;
             }
-            inputStream.close();
-            outputStream.close();
-        } catch (IOException e) {
+            if(!contiene){
+                System.out.println("No existe en el fichero.");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (NullPointerException e) {
+            System.out.println(e.toString() + "Seleccione un archivo primero");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            if (entradaScanner != null) {
+                entradaScanner.close();
+            }
+        }
+        System.out.println(text);
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter("destino.txt");
+            pw = new PrintWriter(fichero);
+            pw.println(text);
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (null != fichero)
+                    fichero.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
         }
     }
 }
--- Examen_PSP/src/es/florida/App2.java
+++ Examen_PSP/src/es/florida/App2.java
@@ -1,4 +1,7 @@
 package es.florida;
 
 public class App2 {
+    public static void main(String[] args) {
+
+    }
 }
